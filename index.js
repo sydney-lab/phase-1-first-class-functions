246695ed2bfdefe2eac5bf15c4bafbdf7b06d8e4
sydney <sydneyosindi03@gmail.com>
2025-06-12 02:07:26 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback) {
+    callback(); 
+}
+  function returnsANamedFunction() {
+    function namedFunction() {
+      console.log("I'm a named function!");
+    }
+    return namedFunction;
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function() {
+      console.log("I'm an anonymous function!");
+    };
+  }
+  
